ede5cdbaed60b210671a0441701be2a42ae447fb
Nick Mistretta <nickmistretta9@gmail.com>
2018-04-03 16:39:47 -0400

    Initial commit

---
+++ WordUnscrambler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wordUnscrambler = new WordUnscrambler();
+            wordUnscrambler.Run();
+        }
+    }
+}
---
+++ WordUnscrambler/WordUnscrambler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordUnscrambler
+{
+    public class WordUnscrambler
+    {
+        private List<string> _scrambledWords;
+
+        public WordUnscrambler()
+        {
+            _scrambledWords = new List<string>();
+        }
+
+        public void PromptForInput()
+        {
+            Console.Write("Would you like to input words manually or from a file? ");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "manual":
+                    AddWordsManual();
+                    break;
+                case "file":
+                    AddWordsFromFile();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void AddWordsManual()
+        {
+            var input = string.Empty;
+            do
+            {
+                Console.Write("Enter the word you would like to unscramble or enter nothing to exit: ");
+                input = Console.ReadLine();
+
+                _scrambledWords.Add(input);
+            }
+            while (!input.Equals(String.Empty));
+        }
+
+        public void AddWordsFromFile()
+        {
+            Console.Write("Enter the path for the file: ");
+            var input = Console.ReadLine();
+        }
+
+        public void Run()
+        {
+            PromptForInput();
+        }
+    }
+}
